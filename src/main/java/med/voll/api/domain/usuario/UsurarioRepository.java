ebdc508f1cda7b9a9744915403067c10b13c95e6
ronaldosnavarro <navarro.silva@terra.com.br>
2024-07-15 18:03:19 -0300

    Implementando repository e service

--- src/main/java/med/voll/api/domain/usuario/UsurarioRepository.java
+++ src/main/java/med/voll/api/domain/usuario/UsurarioRepository.java
@@ -1,4 +1,8 @@
 package med.voll.api.domain.usuario;
 
-public interface UsurarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsurarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
